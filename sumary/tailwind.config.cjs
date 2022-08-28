73c81208ad89b0fc28bf16b1152691d19e1ba556
thepza <gan14tap@gmail.com>
2022-08-28 10:41:47 +0700

    Order Summary V1

---
+++ sumary/tailwind.config.cjs
@@ -0,0 +1,23 @@
+
+module.exports = {
+  content: ["./*.{vue,js,ts,jsx,html}"],
+  theme: {
+    colors: {
+      pale_blue: 'hsl(225, 100%, 94%)',
+      bright_blue: 'hsl(245, 75%, 52%)',
+      very_blue: 'hsl(225, 100%, 98%)',
+      des_blue: 'hsl(224, 23%, 55%)',
+      dark_blue: 'hsl(223, 47%, 23%)',
+      
+    },
+    fontSize: {
+      'body': '16px',
+      'body1': '24px',
+    },
+    fontFamily: {
+      'hat': ['Red Hat Display'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
